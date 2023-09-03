8ed70f5bd62c16d3ea0f33a1fa33813ae3dd5050
Alexandre Stumpf <als.stumpf@gmail.com>
2023-09-03 03:55:26 -0300

    manipulando multiplas requisições em paralelo

--- javaScript/pokedex/assets/js/main.js
+++ javaScript/pokedex/assets/js/main.js
@@ -1,14 +1,17 @@
+function convertPokemonTypes(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class = "type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemon (pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
+                    ${convertPokemonTypes(pokemon.types).join('')}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Imagem ${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="Imagem ${pokemon.name}">
             </div>
         </li>
     `;
@@ -17,5 +20,11 @@ function convertPokemon (pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons().then((pokemonsList = []) => {
-    pokemonList.innerHTML += pokemonsList.map(convertPokemon).join('');
+
+    const newHtml = pokemonsList.map(convertPokemon).join('');
+    pokemonList.innerHTML = newHtml;
+    
 })
+
+
+//Pokémon: 151 na Geração I na região de Kanto; 100 na Geração II na região de Johto; 135 na Geração III na região de Hoenn; 107 na Geração IV na região de Sinnoh; 156 na Geração V na região de Unova; 72 na Geração VI na região de Kalos; 88 na Geração VII nas regiões de Alola e Kanto; 91 na Geração VIII na região de Galar e Hisui, e 103 na Geração IX na região de Paldea.
--- javaScript/pokedex/assets/js/poke-api.js
+++ javaScript/pokedex/assets/js/poke-api.js
@@ -1,8 +1,16 @@
 const pokeApi = {}
+pokeApi.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonbody) => jsonbody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
         .catch((error) => console.error(error))
 }
