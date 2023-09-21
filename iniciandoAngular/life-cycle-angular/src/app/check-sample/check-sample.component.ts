1ec7adf947ec7bedba9c65ae519498d3f0612a45
Alexandre Stumpf <als.stumpf@gmail.com>
2023-09-20 23:57:21 -0300

    ordem de execução dos sub eventos do doChecked

--- iniciandoAngular/life-cycle-angular/src/app/check-sample/check-sample.component.ts
+++ iniciandoAngular/life-cycle-angular/src/app/check-sample/check-sample.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-check-sample',
   templateUrl: './check-sample.component.html',
   styleUrls: ['./check-sample.component.css']
 })
-export class CheckSampleComponent implements OnInit {
+export class CheckSampleComponent implements OnInit,
+           DoCheck,
+           AfterContentChecked,
+           AfterContentInit,
+           AfterViewChecked,
+           AfterViewInit {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor segunda leva')
+   }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit')
   }
 
 }
