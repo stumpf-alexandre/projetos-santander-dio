4d11aeb2895137e65f780f18a5ce8f5f1d858a1a
Alexandre Stumpf <als.stumpf@gmail.com>
2023-09-20 23:33:24 -0300

    implementando OnChange e vendo como é sua execução no console.log

--- iniciandoAngular/life-cycle-angular/src/app/title/title.component.ts
+++ iniciandoAngular/life-cycle-angular/src/app/title/title.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  nome: string = 'Alexandre'
-  constructor() { }
+  //inputando o valor de nome
+  @Input() nome: string = ''
+
+  constructor() {
+    console.log(`Constructor ${this.nome}`);
+   }
+  ngOnChanges(): void {
+    console.log(`OnChange ${this.nome}`);
+  }
 
   ngOnInit(): void {
-    console.log("Ola eu sou o nascimento do meu titulo component");
-    this.nome = `Ola ${this.nome}`;
+    console.log(`OnInit ${this.nome}`);
   }
 
 }
