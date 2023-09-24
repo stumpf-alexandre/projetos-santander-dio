16d87e5d05ee9c0f31a7ea6168b5f4bf2118e0a5
Alexandre Stumpf <als.stumpf@gmail.com>
2023-09-24 00:37:45 -0300

    estilizando as tags com dinamismo

---
+++ iniciandoAngular/diretivasAngular/src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributos',
+  templateUrl: './comp-atributos.component.html',
+  styleUrls: ['./comp-atributos.component.css']
+})
+export class CompAtributosComponent implements OnInit {
+  estilo: string = "disable"
+  corFundo: string = "blue"
+  corFonte: string = "yellow"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  trocar() {
+    if (this.estilo === "disable") {
+      this.estilo = "enable"
+    } else {
+      this.estilo = "disable"
+    }
+  }
+
+}
