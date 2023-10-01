acb7f025d072c4bbc56d67bb524ceab8cf6a289f
Alexandre Stumpf <als.stumpf@gmail.com>
2023-10-01 20:54:22 -0300

    criando outra pagina, deixando-a dinamica e pegando os dados em um banco de dado fake

---
+++ blogAngular/src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+//banco de dados ficticios
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://melhoreseusresultados.com.br/wp-content/uploads/2023/04/Tony-Stark-Substituto-1024x576-1.jpg",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga aliquam accusantium reiciendis ad, numquam sunt quisquam aperiam voluptatem ut tenetur non totam deserunt provident officiis sapiente. Quasi, eos possimus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis"
+  },
+  {
+    "id": "2",
+    "photo": "https://media.seudinheiro.com/uploads/2023/02/Guardians-of-the-Galaxy-Vol.-3-715x402.jpg",
+    "title": "Saiu filme guardiões da galaxia vol 3",
+    "description": " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga aliquam accusantium reiciendis ad, numquam sunt quisquam aperiam voluptatem ut tenetur non totam deserunt provident officiis sapiente. Quasi, eos possimus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis"
+  },
+  {
+    "id": "3",
+    "photo": "https://th.bing.com/th?id=OIP.iS0YZHm_GuNSRTMIsJzhpQHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    "title": "Novo filme guardiões da galaxia vol 2",
+    "description": " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga aliquam accusantium reiciendis ad, numquam sunt quisquam aperiam voluptatem ut tenetur non totam deserunt provident officiis sapiente. Quasi, eos possimus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis"
+  },
+  {
+    "id": "4",
+    "photo": "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/08/06/383152932-i-am-groot.jpg",
+    "title": "Eu sou Groot",
+    "description": " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At fuga aliquam accusantium reiciendis ad, numquam sunt quisquam aperiam voluptatem ut tenetur non totam deserunt provident officiis sapiente. Quasi, eos possimus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur animi sapiente ea magnam, error eius nulla saepe maxime, dignissimos earum esse tenetur ex aut quis dolore debitis sunt beatae nobis"
+  }
+]
