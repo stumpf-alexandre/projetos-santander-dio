b933a50000634d48ebf3dab00d770a6f032dc409
Alexandre Stumpf <als.stumpf@gmail.com>
2023-10-01 21:45:53 -0300

    criando um projeto de rotas com duas paginas

---
+++ rotasAngular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { IndexModule } from './pages/index/index.module';
+import { PortfolioModule } from './pages/portfolio/portfolio.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IndexModule,
+    PortfolioModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
