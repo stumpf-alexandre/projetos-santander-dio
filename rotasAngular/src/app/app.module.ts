e696889e88772a2d1bd5821ebae4fe8cd9c0ff32
Alexandre Stumpf <als.stumpf@gmail.com>
2023-10-05 21:47:06 -0300

    navegando entre paginas com routerLink

--- rotasAngular/src/app/app.module.ts
+++ rotasAngular/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import { IndexModule } from './pages/index/index.module';
 import { PortfolioModule } from './pages/portfolio/portfolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
