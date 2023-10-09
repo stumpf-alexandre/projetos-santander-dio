a48395be07275e02357b44f07c3503201b72cef8
Alexandre Stumpf <als.stumpf@gmail.com>
2023-10-08 21:19:09 -0300

    criando uma pokedex com angular e guardando valores de variaveis no environment

---
+++ servicesAngular/src/app/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  name: string = "BULBASSAUR"
+  attributesTypes: string[] = ['FIRE', 'ROCK']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
