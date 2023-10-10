fca65c2effd09a8bfc52fbfa9b86db7343f4f14c
Alexandre Stumpf <als.stumpf@gmail.com>
2023-10-10 00:33:56 -0300

    tipando os dados

--- servicesAngular/src/app/components/card/card.component.ts
+++ servicesAngular/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
@@ -7,17 +8,34 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name: string = "BULBASSAUR"
+  pokemon: PokemonData
+  name: string = "BULBASAUR"
   attributesTypes: string[] = ['FIRE', 'ROCK']
 
   constructor(
     private service: PokemonService
-  ) { }
+  ) {
+    this.pokemon = {
+      id: 0,
+      name: '',
+      sprites: {
+        front_default: ''
+      },
+      types: []
+    }
+  }
 
   ngOnInit(): void {
-    this.service.getPokemon('bulbasauro').subscribe(
+    this.service.getPokemon('pikachu').subscribe(
       {
-        next: (res) => console.log(res),
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+        },
         error: (err) => console.log(err)
       }
     )
