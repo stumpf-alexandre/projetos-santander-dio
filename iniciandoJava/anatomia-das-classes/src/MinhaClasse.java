4934937f4b26b3537e63a455ac762793b15d4627
Alexandre Stumpf <als.stumpf@gmail.com>
2023-10-15 23:01:27 -0300

    declarando variaveis e métodos

---
+++ iniciandoJava/anatomia-das-classes/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Ola turma, sejam bem-vindos");
+        String primeiroNome = "Alexandre";
+        String segundoNome = "Stumpf";
+
+        String nome = nomeCompletos(primeiroNome, segundoNome);
+        System.out.println(nome);
+    }
+
+    public static String nomeCompletos(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
